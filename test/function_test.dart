1dda57c6e4969fc556714643c0742d8a7444a494
Tobe O <thosakwe@gmail.com>
2018-05-21 03:48:22 -0400

    Fixed travis

--- test/function_test.dart
+++ test/function_test.dart
@@ -2,14 +2,16 @@ import 'dart:math';
 import 'package:tensorflow/tensorflow.dart' as tf;
 import 'package:test/test.dart';
 
+var truncPi = pi.truncateToDouble();
+
 tf.Output<double> circleArea(tf.Output<double> radius) =>
-    tf.constant(pi) * tf.square(radius);
+    tf.constant(truncPi) * tf.square(radius);
 
 void main() {
   test('composite ops', () {
     expect(
       circleArea(tf.constant(4.0)).run(),
-      pi * pow(4.0, 2),
+      truncPi * pow(4.0, 2),
     );
   });
 }
