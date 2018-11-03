67de32b3c9ad58d4a2746ee56b1fbe451a3ff42e
Tobe O <thosakwe@gmail.com>
2018-11-03 14:10:47 -0400

    update travis

--- tool/.travis.sh
+++ tool/.travis.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 set -e
 pub get
-pub global activate scripts
-alias scripts="pub global run scripts"
-scripts build
-scripts clean
+
 DEFAULT_NUM_MAKE_JOBS=2
 
 # https://github.com/travis-ci/travis-ci/issues/4696#issuecomment-308517449
 NUM_MAKE_JOBS=$(($(nproc 2> /dev/null || echo ${DEFAULT_NUM_MAKE_JOBS})+1))
 
+# Build the library
+mkdir -p cmake-build-debug
+cd cmake-build-debug
+cmake ..
+cmake --build . -- "-j${NUM_MAKE_JOBS}"
+cd ..
+
+# Now, just run our tests with the newly-built library.
 pub run test "-j${NUM_MAKE_JOBS}"
